bac18368f635c02c13158081400eda81c43f11b7
Adepetun22 <adepetun22@gmail.com>
2025-12-22 00:01:31 +0100

    New page Category

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import ProductDetails from './Pages/ProductDetails';
+import Category from './Pages/Category';
 import DesktopNav from './Components/Navigation/DesktopNav';
 import FooterComponent from './Components/Footer/FooterComponent';
 import { CartProvider } from './CartContext';
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/ProductDetails" element={<ProductDetails />} />
+          <Route path="/Category" element={<Category />} />
         </Routes>
         <FooterComponent />
       </Router>
--- src/Components/EmailSubscription.jsx
+++ src/Components/EmailSubscription.jsx
@@ -12,8 +12,8 @@ const EmailSubscription = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 mb-12 xs:mb-8 sm:mb-16">
-      <div className="bg-black rounded-2xl py-6 xs:py-4 sm:py-9 px-8 xs:px-4 sm:px-16 flex flex-col xs:flex-col sm:flex-row flex-wrap content-center items-center justify-between self-stretch shrink-0 relative overflow-hidden">
+    <div className="flex justify-center w-full">
+      <div className="bg-black rounded-2xl py-6 xs:py-4 sm:py-9 px-8 xs:px-4 sm:px-16 flex flex-col xs:flex-col sm:flex-row flex-wrap content-center items-center justify-between shrink-0 relative overflow-hidden max-w-[1242px] w-full">
         <div className="text-white text-left text-2xl xs:text-xl sm:text-[2.5rem] leading-8 xs:leading-6 sm:leading-11.25 font-normal relative flex-1 min-w-60 xs:min-w-48 sm:min-w-72.5 mb-6 sm:mb-0">
           STAY UPTO DATE ABOUT OUR LATEST OFFERS
         </div>
